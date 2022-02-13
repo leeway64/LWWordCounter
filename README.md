489902108d91ed8ae57e6d65a893deb398aa297a
Lee-Way Wang <lee.way56@comcast.net>
2022-02-12 17:01:53 -0800

    Input schema, CMakeLists, README
    - Rename input schema
    - Install Cap’n Proto with Conan in CMakeLists
    - Update README and gitignore

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # LWWordCounter
 
 LWWordCounter is an application that visualizes the frequency of words from a text file.
+
+## Installation
+
+The simplest way to use LWWordCounter is to run the binary from the [releases]() page.
+Alternatively, you can build it on your own machine.
+
+Note: if the CMake output says that a certain package is `locked by another concurrent conan
+process, wait...`, then run `conan remove -f <package_name>/<package_version>` in the terminal.
+
+## Usage
+
+## Example
+
+## Third-Party Tools
+
+- Catch2: 
+- nlohmann_json: 
+- 
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
 
+#include <nlohmann/json.hpp>
+#include <nlohmann/json-schema.hpp>
+
+using nlohmann::json_schema::json_validator;
 
 int main()
 {
-    const std::string timers_file_name = "list_of_timers.json";
+    const std::string input_schema_name = "user_input_schema.json";
+    const std::string input_file_name = "input_file.json";
 
-    const std::filesystem::path timers_file{ timers_file_name };
-    if (!std::filesystem::exists(timers_file))
+    const std::filesystem::path input_file{ input_file_name };
+    if (!std::filesystem::exists(input_file))
     {
-        std::ofstream output(timers_file_name);
+        std::cout << "No input JSON file found" << std::endl;
+        std::cout << "Creating a new JSON file" << std::endl;
+
+        std::ofstream output(input_file_name);
         // Write brackets so that the file can be correctly read as a JSON file
-        output << "{\n}\n";
+        output << "{\n    \n}\n";
         output.close();
     }
-	std::cout << "hello" << std::endl;
+    else
+    {
+        std::ifstream input(input_file_name);
+        nlohmann::json input_file_json;
+        input >> input_file_json;
+        input.close();
+
+        std::ifstream input(input_schema_name);
+        nlohmann::json input_schema_json;
+        input >> input_schema_json;
+        input.close();
+
+        json_validator validator;
+
+        validator.set_root_schema(user_input_schema);
+        try {
+            validator.validate(input_file_json);
+            std::cout << "Validation succeeded\n";
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Validation failed, here is why: " << e.what() << std::endl;
+        }
+    }
 	return 0;
 }
