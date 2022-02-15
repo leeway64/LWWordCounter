e2e6ef98a98467080e3b5961c38865dd1f237b17
Lee-Way Wang <lee.way56@comcast.net>
2022-02-14 21:34:41 -0800

    JSON files, toml11
    - Retrieve input JSON and input schema files
    - Add toml11 using Conan

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 
 #include <nlohmann/json.hpp>
 #include <nlohmann/json-schema.hpp>
+#include <toml.hpp>
 
 using nlohmann::json_schema::json_validator;
 
 int main()
 {
-    const std::string input_schema_name = "user_input_schema.json";
+    const std::string input_schema_name = "../schema/user_input_schema.json";
     const std::string input_file_name = "input_file.json";
 
     const std::filesystem::path input_file{ input_file_name };
@@ -20,31 +21,35 @@ int main()
 
         std::ofstream output(input_file_name);
         // Write brackets so that the file can be correctly read as a JSON file
-        output << "{\n    \n}\n";
+        output << "{\n}\n";
         output.close();
     }
     else
     {
-        std::ifstream input(input_file_name);
+        // Get the input JSON file
+        std::ifstream input1(input_file_name);
         nlohmann::json input_file_json;
-        input >> input_file_json;
-        input.close();
+        input1 >> input_file_json;
+        input1.close();
 
-        std::ifstream input(input_schema_name);
+        // Get the schema for the input JSON file
+        std::ifstream input2(input_schema_name);
         nlohmann::json input_schema_json;
-        input >> input_schema_json;
-        input.close();
+        input2 >> input_schema_json;
+        input2.close();
 
         json_validator validator;
-
-        validator.set_root_schema(user_input_schema);
+        validator.set_root_schema(input_schema_json);
         try {
             validator.validate(input_file_json);
-            std::cout << "Validation succeeded\n";
+            //std::cout << "Validation succeeded\n";
         }
         catch (const std::exception& e) {
             std::cerr << "Validation failed, here is why: " << e.what() << std::endl;
         }
     }
+
+    const toml::value summary_data;
+
 	return 0;
 }
